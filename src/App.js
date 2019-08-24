ca975ef1e589a1b269032ab5819d3a09e07045af
Marek Bojec <marek.bojec@gmail.com>
2019-08-24 18:40:10 +0200

    Adding Content component to main App component and adjusting styling for it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {Copyright} from "./assets/svg/Copyright";
-import {Header} from "./components/Header";
+import {Copyright} from "./assets/svg";
+import {Header, Content} from "./components";
 
 export class App extends Component{
   render() {
     return (
       <div className={'app'}>
         <header className={'app__header'}><Header/></header>
-        <main className={'app__main'}></main>
+        <main className={'app__main'}><Content/></main>
         <footer className={'app__footer'}><Copyright/></footer>
       </div>
     )
