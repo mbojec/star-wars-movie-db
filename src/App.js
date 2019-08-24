b61bab3cb0b11fdc67507f7c9be6cbd16894443c
Marek Bojec <marek.bojec@gmail.com>
2019-08-24 13:17:40 +0200

    Adding basic layout structure with simple styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export class App extends Component{
   render() {
     return (
-      <header><h1>Hello World</h1></header>
+      <div className={'app'}>
+        <header className={'app__header'}></header>
+        <main className={'app__main'}></main>
+        <footer className={'app__footer'}></footer>
+      </div>
     )
   }
 }
