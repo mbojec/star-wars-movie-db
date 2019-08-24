5ea048cb59bc4567d03e6c1fc84fc908c083bb8e
Marek Bojec <marek.bojec@gmail.com>
2019-08-24 20:34:28 +0200

    Adding redux package and creating store, reducer and actions file for fetching films from Rest API

---
+++ src/redux/reducer.js
@@ -0,0 +1,25 @@
+import {FETCH_FILMS_SUCCESS, FETCHING_FILMS} from "./actions";
+
+const initialState = {
+  films: [],
+  isLoading: true
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_FILMS:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case FETCH_FILMS_SUCCESS:
+      return {
+        films: action.films,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+};
+
+export {reducer};
