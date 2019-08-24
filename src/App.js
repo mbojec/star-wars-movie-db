c076a061301111fb9d8ba02427806beba7b6027a
Marek Bojec <marek.bojec@gmail.com>
2019-08-24 13:58:01 +0200

    Adding Logo and Copyright svg assets and adding them to header and footer section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import {Logo} from "./assets/svg/Logo";
+import {Copyright} from "./assets/svg/Copyright";
 
 export class App extends Component{
   render() {
     return (
       <div className={'app'}>
-        <header className={'app__header'}></header>
+        <header className={'app__header'}><Logo/></header>
         <main className={'app__main'}></main>
-        <footer className={'app__footer'}></footer>
+        <footer className={'app__footer'}><Copyright/></footer>
       </div>
     )
   }
