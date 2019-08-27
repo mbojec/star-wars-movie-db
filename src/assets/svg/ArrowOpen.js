0560cd1c0409deacb53d38ba664567565a6e8728
Marek Bojec <marek.bojec@gmail.com>
2019-08-27 17:28:23 +0200

    Creating separate stylesheet for cards and splitting it with list stylesheet

--- src/assets/svg/ArrowOpen.js
+++ src/assets/svg/ArrowOpen.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const ArrowOpen = () => (
-  <svg className={'list-item__icon__arrow'} width={18} height={18} fill="none">
-    <circle className={'list-item__icon__arrow--background'} cx={9} cy={9} r={9} fill='#00687F'/>
+  <svg className={'card__icon__arrow'} width={18} height={18} fill="none">
+    <circle className={'card__icon__arrow--background'} cx={9} cy={9} r={9} fill='#00687F'/>
     <path
       d="M5.25 7.313L9 11.26l3.75-3.947"
       stroke="#fff"
