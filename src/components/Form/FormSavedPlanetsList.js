503e6c5d8bf781634af1e229cf5b5ea967d79439
Marek Bojec <marek.bojec@gmail.com>
2019-08-27 21:05:23 +0200

    Converting class component to function components

--- src/components/Form/FormSavedPlanetsList.js
+++ src/components/Form/FormSavedPlanetsList.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {Delete} from "../../assets/svg";
 import {withRedux} from "../../redux/wrapper";
 
-class FormSavedPlanetsList extends Component{
-  deletePlanet(planet){
-    this.props.onDeleteQueryPlanet(planet);
+const FormSavedPlanetsList = ({savedQueryPlanets, onDeleteQueryPlanet}) => {
+  function deletePlanet(planet){
+    onDeleteQueryPlanet(planet);
   }
 
-  render() {
-    return(
-      <div className={'row'}>
-        {this.props.savedQueryPlanets.map((singlePlanet) =>
-          <div key={singlePlanet.name} className={'col-6 col-xs-4 col-sm-3  form__save-list-section__planet-container'}>
-            <div className={'form__save-list-section__planet'}>
-              <p>{singlePlanet.name}</p>
-              <span onClick={() => this.deletePlanet(singlePlanet)}><Delete/>
+  return(
+    <div className={'row'}>
+      {savedQueryPlanets.map((singlePlanet) =>
+        <div key={singlePlanet.name} className={'col-6 col-xs-4 col-sm-3  form__save-list-section__planet-container'}>
+          <div className={'form__save-list-section__planet'}>
+            <p>{singlePlanet.name}</p>
+            <span onClick={() => deletePlanet(singlePlanet)}><Delete/>
               </span>
-            </div>
-          </div>)}
-      </div>
-    )
-  }
-}
+          </div>
+        </div>)}
+    </div>
+  )
+
+};
 
 
 FormSavedPlanetsList.propTypes = {
--- src/components/PlanetTable/PlanetTableHeader.js
+++ src/components/PlanetTable/PlanetTableHeader.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {Sort} from "../../assets/svg";
 
-class PlanetTableHeader extends Component{
+const PlanetTableHeader = ({label, keyValue, onPress, sortedByHighest, sortedCategory}) => {
 
-  handleClick(){
-    this.props.onPress(this.props.keyValue);
+  function handleClick() {
+    onPress(keyValue);
   }
-
-  render() {
-    return(
-      <th className={`${this.props.keyValue === 'name'? 'table__planet-name' : 'table__header'}`}>
-        <div className={`${this.props.keyValue === 'name'? 'table__header-container--planet' : 'table__header-container'}`}>
-          <p className={'table__header__label'}>{this.props.label}</p>
-          <span className={`table__header__icon 
-          ${this.props.sortedCategory===this.props.keyValue && 
-          (this.props.sortedByHighest? 'table__header__icon__selected--highest':'table__header__icon__selected--lowest')}`}
-                onClick={() => this.handleClick()}><Sort/>
+  return(
+    <th className={`${keyValue === 'name'? 'table__planet-name' : 'table__header'}`}>
+      <div className={`${keyValue === 'name'? 'table__header-container--planet' : 'table__header-container'}`}>
+        <p className={'table__header__label'}>{label}</p>
+        <span className={`table__header__icon 
+          ${sortedCategory===keyValue &&
+        (sortedByHighest? 'table__header__icon__selected--highest':'table__header__icon__selected--lowest')}`}
+              onClick={() => handleClick()}><Sort/>
           </span>
-        </div>
-      </th>
-    )
-  }
-}
+      </div>
+    </th>
+  )
+};
 
 PlanetTableHeader.propTypes = {
   label: PropTypes.string,
