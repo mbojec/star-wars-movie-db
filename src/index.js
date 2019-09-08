bb8682d35da22b2ae7dcd60582488f66ec3497a0
Marek Bojec <marek.bojec@gmail.com>
2019-09-08 18:53:28 +0200

    Editting webpack and renaming scss dir to styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
-import './scss/main.scss';
+import './styles/main.scss';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
--- webpack.config.js
+++ webpack.config.js
@@ -1,171 +1,160 @@
-const path = require("path");
-const entryPath = "src";
-const entryFile = "index.js";
+const path = require('path');
+const entryPath = 'src';
+const entryFile = 'index.js';
 const Html = require('html-webpack-plugin');
-const MiniCSS = require("mini-css-extract-plugin");
+const MiniCSS = require('mini-css-extract-plugin');
 
-module.exports = function (env) {
-    let isDev = !!env.dev;
-    let isProd = !!env.prod;
-    const config = {};
+module.exports = function(env) {
+  let isDev = !!env.dev;
+  let isProd = !!env.prod;
+  const config = {};
 
-    config.entry = `./${entryPath}/${entryFile}`;
+  config.entry = `./${entryPath}/${entryFile}`;
 
-    config.output = {
-        filename: isDev ? "out.js" : "out.[chunkhash].js",
-        path: path.resolve(__dirname, `${entryPath}/build`),
-        publicPath: '/'
-    };
+  config.output = {
+    filename: isDev ? 'out.js' : 'out.[chunkhash].js',
+    path: path.resolve(__dirname, `${entryPath}/build`),
+    publicPath: '/',
+  };
 
-    config.mode = isProd? "production" : "development";
-    config.devtool = isProd? false : "source-map";
+  config.mode = isProd ? 'production' : 'development';
+  config.devtool = isProd ? false : 'source-map';
 
+  config.module = {};
+  config.module.rules = [];
 
-    config.module = {};
-    config.module.rules = [];
+  const browsers = {
+    dev: ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version'],
+    prod: ['>0.2%', 'not dead', 'not op_mini all'],
+  };
 
-    const browsers = {
-        dev: ["last 1 chrome version",
-            "last 1 firefox version",
-            "last 1 safari version"],
-        prod: [">0.2%",
-            "not dead",
-            "not op_mini all"]
-    };
+  const js = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: isDev ? browsers.dev : browsers.prod,
+                },
+              },
+            ],
+          ],
+          plugins: ['@babel/plugin-syntax-dynamic-import'],
+        },
+      },
+      'eslint-loader',
+    ],
+  };
 
-    const js = {
-        test: /\.js$/, exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
+  const css = {
+    test: /\.css/,
+    use: ['style-loader', 'css-loader'],
+  };
+
+  const scss = {
+    test: /\.scss$/,
+    use: [
+      isProd
+        ? MiniCSS.loader
+        : {
+            loader: 'style-loader',
             options: {
-                presets: [
-                    ["@babel/preset-env", {
-                        targets: {
-                            browsers: isDev ? browsers.dev : browsers.prod
-                        }
-                    }]
-                ],
-                plugins: [
-                    '@babel/plugin-syntax-dynamic-import'
-                ]
-            }
-          }, "eslint-loader"
-        ]
-    };
-
-    const css = {
-        test: /\.css/,
-        use: ['style-loader', 'css-loader']
-    };
-
-    const scss = {
-        test: /\.scss$/,
-        use: [
-            isProd
-                ? MiniCSS.loader
-                :
-                {   loader: 'style-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    },
-            {
-                loader: 'css-loader',
-                options: {
-                    sourceMap: !isProd,
-                }
+              sourceMap: true,
             },
-            {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: () => [
-                        require('precss'),
-                        require('autoprefixer')({
-                            browsers: isProd
-                                ? browsers.prod : browsers.dev
-                        })
-                    ]
-                }
-            },
-            {
-                loader: 'sass-loader',
-                options: {
-                    sourceMap: !isProd,
-                    minimize: isProd
-                }
-            },
-        ]
+          },
+      {
+        loader: 'css-loader',
+        options: {
+          sourceMap: !isProd,
+        },
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: () => [
+            require('precss'),
+            require('autoprefixer')({
+              browsers: isProd ? browsers.prod : browsers.dev,
+            }),
+          ],
+        },
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: !isProd,
+          minimize: isProd,
+        },
+      },
+    ],
+  };
+
+  const images = {
+    test: /\.(jpg|jpeg|gif|png|csv)$/,
+    use: {
+      loader: 'file-loader',
+      options: {
+        name: isProd ? '[name].[hash].[ext]' : '[name].[ext]',
+        publicPath: 'images',
+        outputPath: 'images',
+      },
+    },
+  };
+
+  const fonts = {
+    test: /\.(eot|ttf|woff|woff2)$/,
+    use: {
+      loader: 'file-loader',
+      options: {
+        name: isProd ? '[name].[hash].[ext]' : '[name].[ext]',
+        publicPath: `${entryPath}/assets/fonts`,
+        outputPath: `${entryPath}/assets/fonts`,
+      },
+    },
+  };
+
+  const svg = {
+    test: /\.svg$/,
+    loader: 'svg-inline-loader',
+  };
+
+  config.module.rules.push(js);
+  config.module.rules.push(scss);
+  config.module.rules.push(images);
+  config.module.rules.push(fonts);
+  config.module.rules.push(svg);
+  config.module.rules.push(css);
+
+  config.plugins = [];
+
+  const html = new Html({
+    filename: 'index.html',
+    template: `./${entryPath}/index.html`,
+    minify: isProd ? { collapseWhitespace: true } : false,
+  });
+
+  if (isProd) {
+    config.plugins.push(new MiniCSS({ filename: 'main.scss.[chunkhash].css' }));
+  }
+
+  config.plugins.push(html);
+
+  if (isDev) {
+    config.devServer = {
+      contentBase: path.join(__dirname, `./${entryPath}`),
+      port: 3001,
+      compress: true,
+      progress: true,
+      overlay: true,
+      historyApiFallback: true,
     };
+  }
 
-    const images = {
-        test: /\.(jpg|jpeg|gif|png|csv)$/,
-        use: {
-            loader: 'file-loader',
-            options: {
-                name: isProd
-                    ? '[name].[hash].[ext]'
-                    : '[name].[ext]',
-                publicPath: 'images',
-                outputPath: 'images'
-            }
-        }
-    };
-
-    const fonts = {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: {
-            loader: 'file-loader',
-            options: {
-                name: isProd
-                    ? '[name].[hash].[ext]'
-                    : '[name].[ext]',
-                publicPath: `${entryPath}/assets/fonts`,
-                outputPath: `${entryPath}/assets/fonts`
-            }
-        }
-    };
-
-    const svg = {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader'
-    };
-
-    config.module.rules.push(js);
-    config.module.rules.push(scss);
-    config.module.rules.push(images);
-    config.module.rules.push(fonts);
-    config.module.rules.push(svg);
-    config.module.rules.push(css);
-
-    config.plugins = [];
-
-    if(isProd) {
-        config.plugins.push( new MiniCSS(
-            { filename: 'main.scss.[chunkhash].css' } )
-        )
-    }
-
-    config.plugins.push( new Html({
-            filename: 'index.html',
-            template: `./${entryPath}/index.html`,
-            minify: isProd
-                ? { collapseWhitespace: true }
-                : false
-        })
-    );
-
-
-    if(isDev) {
-        config.devServer = {
-            contentBase: path.join(__dirname, `./${entryPath}`),
-            port: 3001,
-            compress: true,
-            progress: true,
-            overlay: true,
-            historyApiFallback: true,
-        }
-    }
-
-    return config;
+  return config;
 };
